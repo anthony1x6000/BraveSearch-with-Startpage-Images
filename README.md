4b71a41907d70fb06e1fc9262b6ba1c959a924d2
anthop <33004321+anthony1x6000@users.noreply.github.com>
2023-04-30 20:18:42 -0400

    duckduckgo support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Brave Search Startpage Image Service
+# Brave Search with Startpage, DuckDuckGo as an Image Service
 Install with TamperMonkey by [viewing raw of 'startpageImagesBraveSearch.user.js'](https://github.com/anthony1x6000/BraveSearch-with-Startpage-Images/raw/main/startpageImagesBraveSearch.user.js) \
- Use images provided by StartPage in Brave Search \
+ Use images provided by other services in Brave Search \
  Better than using Bing or Google. \
 ![preview](braveImages.webp)
 I don't know why Brave didn't renew their contract or whatever they had with Microsoft before removing images from Brave Search... Maybe Microsoft didn't want to renew. \
--- startpageImagesBraveSearch.user.js
+++ startpageImagesBraveSearch.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Brave to Startpage Images
+// @name         Brave to StartPage DuckDuckGo Images
 // @namespace    https://github.com/anthony1x6000/BraveSearch-with-Startpage-Images
 // @version      0.1
-// @description  Use StartPage as an image service for Brave Search.
+// @description  Use StartPage or other services as an image service for Brave Search.
 // @author       anthony1x6000
 // @match        https://search.brave.com/search?q=*
 // @icon         https://brave.com/static-assets/images/brave-favicon.png
@@ -11,7 +11,6 @@
 
 (function() {
     'use strict';
-
     function storeValue() {
         let searchBox = document.querySelector("#searchbox");
         let searchValue = searchBox.value;
@@ -21,15 +20,20 @@
     let popup;
     let imgredir;
     let SPpreference;
+    let imageService = "duckdg";
     let tabImages = document.querySelector("#tab-images");
     tabImages.onclick = function() {
-        //SPpreference = "prfe=" + "adfa3411ba8020cd76c17db84e9cb1bb72ed463f538f4b3f308bc6f294facedabfc8d5d231f716d048f5419444e3f969f432445f5268362ac9d1d9ad03bf78892908f9f6d0a683f079b4f2d1";
-        if (SPpreference) {
-            imgredir = `https://www.startpage.com/sp/search?${SPpreference}&query=${storeValue()}&cat=images`;
-        } else {
-            imgredir = `https://www.startpage.com/sp/search?&query=${storeValue()}&cat=images`;
+        switch (imageService) {
+            case "spage":
+                var SPpreference;
+                imgredir = `https://www.startpage.com/sp/search?&query=${storeValue()}&cat=images`;
+                break;
+            case "duckdg":
+                imgredir = `https://duckduckgo.com/?hps=1&q=${storeValue()}&iax=images&ia=images`;
+                break;
+            default:
+                imgredir = `https://www.startpage.com/sp/search?&query=${storeValue()}&cat=images`;
         }
-
         window.open(imgredir);
         popup = document.querySelector("#tab-images > div");
         popup.remove();
